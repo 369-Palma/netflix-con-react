ea2b57f9be5616f11daadcc71aa172c40eb73be3
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-12 14:51:39 +0100

    icone inserite

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,44 @@
-import { Container } from "react-bootstrap";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; */
 const NavBar = (props) => {
   return (
-    <Container className="d-flex">
-      <nav
-        className="navbar navbar-expand-lg navbar-dark"
-        /* {style="background-color: #221f1f"} */
-      >
-        <a className="navbar-brand" href={props.href}>
-          <img src="../../assets/logo.png" alt="logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a href={props.href}>
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a href={props.href}>TV Shows</a>
-            </li>
-            <li className="nav-item">
-              <a href={props.href}>Movies</a>
-            </li>
-            <li className="nav-item">
-              <a href={props.href}>Recently Added</a>
-            </li>
-            <li className="nav-item">
-              <a href={props.href}>My List</a>
-            </li>
-          </ul>
-          <i className="fa fa-search icons"></i>
-          <div id="kids">KIDS</div>
-          <i className="fa fa-bell icons"></i>
-          <i className="fa fa-user icons"></i>
-        </div>
-      </nav>
-    </Container>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="../../assets/logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">TV Shows</Nav.Link>
+            <Nav.Link href="#pricing">Recently Added</Nav.Link>
+            <Nav.Link href="#pricing">My List </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">
+              <i class="fa fa-search icons"></i>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" id="kids">
+              {" "}
+              KIDS
+            </Nav.Link>
+            <Nav.Link href="#deets">
+              <i class="fa fa-bell icons"></i>
+            </Nav.Link>
+            <Nav.Link href="#deets">
+              <i class="fa fa-user icons"></i>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
